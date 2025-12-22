1d9bf3a942952d3206e41cb7a8a9ebd489667e39
Hariom <hariom@byosync.in>
2025-12-22 22:28:17 +0530

    Updated Consent View

--- ByoSync-User/User/Views/Authentication+Login+Register+Consent/ Auth+Login+Register/AuthenticationView.swift
+++ ByoSync-User/User/Views/Authentication+Login+Register+Consent/ Auth+Login+Register/AuthenticationView.swift
@@ -144,23 +144,23 @@ struct AuthenticationView: View {
             .navigationDestination(isPresented: $openEnterNumber) {
                 EnterNumberView()
             }
-//            .navigationDestination(isPresented: $openTestingView) {
-//                #if DEBUG
-//                UserDataByIdView(mode: .mockContent)
-//                #else
-//                UserDataByIdView()
-//                #endif
-//            }
+            .navigationDestination(isPresented: $openTestingView) {
+                #if DEBUG
+                UserConsentView(onComplete: {
+                    print("Consent Given!!")
+                })
+                #endif
+            }
             .alert(deviceAlertMessage, isPresented: $showDeviceAlert) {
                 Button("OK", role: .cancel) { }
             }
-//            .toolbar{
-//                Button{
-//                    openTestingView.toggle()
-//                }label: {
-//                    Text("Testing")
-//                }
-//            }
+            .toolbar{
+                Button{
+                    openTestingView.toggle()
+                }label: {
+                    Text("Testing")
+                }
+            }
             
             // 🔁 Decide what to do when API call finishes
             .onChange(of: deviceRegistrationVM.isLoading) { isLoading in
