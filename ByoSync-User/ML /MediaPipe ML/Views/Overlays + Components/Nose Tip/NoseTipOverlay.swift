5019134064b75113df5bf57b8b0211b4dc3eacd3
Hariom <hariom@byosync.in>
2025-12-29 22:50:39 +0530

    Updated NoseTip

--- ByoSync-User/ML /MediaPipe ML/Views/Overlays + Components/Nose Tip/NoseTipOverlay.swift
+++ ByoSync-User/ML /MediaPipe ML/Views/Overlays + Components/Nose Tip/NoseTipOverlay.swift
@@ -14,6 +14,7 @@ struct NoseCenterCircleOverlay: View {
     var body: some View {
         GeometryReader { geo in
             Image(systemName: "scope")
+                .fontWeight(.bold)
                 .foregroundStyle(isCentered ? Color.green : Color.red)
                 .frame(width: 10, height: 10)
                 .position(x: geo.size.width / 2,
